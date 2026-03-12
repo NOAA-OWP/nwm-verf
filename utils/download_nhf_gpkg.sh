c8f2cf65d48a03e2fb400e2b333a9fc936f955df
Yuqiong Liu <yuqiong.liu@U-28HBMJQ9PK1NS.nextgenwaterprediction.com>
2026-03-12 16:19:49 -0400

    minor change to download_nhg_gpkg.sh

--- utils/download_nhf_gpkg.sh
+++ utils/download_nhf_gpkg.sh
@@ -2,13 +2,16 @@
 # This script is used to download the NHF GPKG files for different VPUs.
 set -e
 
+id_type="vpu_id" # use "gage_id" for gage-based GPKG files
+version="v1" # specify the version of the NHF GPKG files to download
+
 # conus vpus
 vpus=('01' '02' '03N' '03S' '03W' '04' '05' '06' '07' '08' '09' '10L' '10U' '11' '12' '13' '14' '15' '16' '17' '18')
 
 dest_dir="$HOME/data/hydrofabric/gpkg_nhf/"
 mkdir -p "$dest_dir"
 
-id_type="vpu_id" # use "gage_id" if you want to download by gage ID instead of VPU
+
 for vpu in "${vpus[@]}"; do
     # skip if file already exists
     dest_file="${dest_dir}vpu_${vpu}.gpkg"
@@ -18,6 +21,6 @@ for vpu in "${vpus[@]}"; do
     fi
 
     echo "Downloading GPKG for VPU $vpu..."
-    url="https://edfs.oe.nextgenwaterprediction.com/api/v1/hydrofabric/$vpu/gpkg?id_type=${id_type}"
+    url="https://edfs.oe.nextgenwaterprediction.com/api/${version}/hydrofabric/$vpu/gpkg?id_type=${id_type}"
     curl -fL --progress-bar -o "$dest_file" "$url"
 done
